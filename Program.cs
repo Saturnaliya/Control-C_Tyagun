844407078154e8ca7871f4531f0425904604ece6
Marina Tyagun <saturnaliya@mail.ru>
2023-08-01 18:19:19 +0800

    number of array elements

--- Program.cs
+++ Program.cs
@@ -9,3 +9,12 @@ int n = 4;
 int size = 9;
 string[] arrayOne = new string[size];
 FillRandArr(arrayOne);
+if (GetSizeOfSecondArray(arrayOne) == 0){Console.WriteLine("отсутствуют элементы для ввода в новый массив");}
+else
+{   
+    Console.WriteLine($"Матричный массив и дочерний массив, состоящий из 3(менее 3) элементов:");
+    Console.WriteLine();
+    string[] arrayTwo = RotateElemOfArr(arrayOne);
+    Console.WriteLine($"[{String.Join(", ", arrayOne)}] -> [{String.Join(", ", arrayTwo)}]");
+    Console.WriteLine();
+}
